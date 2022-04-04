1469e82c2ef73622dece31302580b2a31928ebe3
Carlo M <carlo992@gmail.com>
2022-04-04 12:21:52 +0100

    Added prettier config alongside eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
-    node: true
+    node: true,
+    jasmine: true
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error']
+  },
   parserOptions: {
     ecmaVersion: 12
-  },
-  rules: {
   }
 }
